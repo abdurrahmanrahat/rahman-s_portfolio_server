b32bff2859b0e7d21597587fc3037bf76c1f5e53
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-05-12 09:35:40 +0600

    skill route

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
 import { AuthRoutes } from './app/modules/auth/auth.route';
+import { SkillRoutes } from './app/modules/skill/skill.route';
 import { UserRoutes } from './app/modules/user/user.route';
+
 const app: Application = express();
 
 // parsers
@@ -11,6 +13,7 @@ app.use(cors());
 // application routes
 app.use('/api/v1/users', UserRoutes);
 app.use('/api/v1/auth', AuthRoutes);
+app.use('/api/v1/skills', SkillRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
---
+++ src/app/modules/skill/skill.route.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.post('/create-skill');
+
+export const SkillRoutes = router;
