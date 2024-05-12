43f18623e64cca52f3a98e01a3f14547c82ebe57
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-05-12 10:20:52 +0600

    article service function

---
+++ src/app/modules/article/article.service.ts
@@ -0,0 +1,19 @@
+import { TArticle } from './article.interface';
+import { ArticleModel } from './article.model';
+
+// post
+const createArticleIntoDb = async (article: TArticle) => {
+  const result = await ArticleModel.create(article);
+  return result;
+};
+
+// get
+const getAllArticlesFromDb = async () => {
+  const result = await ArticleModel.find();
+  return result;
+};
+
+export const SkillServices = {
+  createArticleIntoDb,
+  getAllArticlesFromDb,
+};
