da545143b9c8fab94365cd25e1211fb7715ca131
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-05-12 10:40:42 +0600

    project service

---
+++ src/app/modules/project/project.service.ts
@@ -0,0 +1,19 @@
+import { TProject } from './project.interface';
+import { ProjectModel } from './project.model';
+
+// post
+const createProjectIntoDb = async (project: TProject) => {
+  const result = await ProjectModel.create(project);
+  return result;
+};
+
+// get
+const getAllProjectsFromDb = async () => {
+  const result = await ProjectModel.find();
+  return result;
+};
+
+export const SkillServices = {
+  createProjectIntoDb,
+  getAllProjectsFromDb,
+};
