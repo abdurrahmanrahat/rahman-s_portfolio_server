4ef39044a068e20ac1db9dadf5305d867651bf7f
abdurrahmanrahat <abdurrahmanrahat47@gmail.com>
2024-05-12 09:48:39 +0600

    global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
+import { globalErrHandler } from './app/middlewares/globalErrHandler';
 import { AuthRoutes } from './app/modules/auth/auth.route';
 import { SkillRoutes } from './app/modules/skill/skill.route';
 import { UserRoutes } from './app/modules/user/user.route';
@@ -18,5 +19,8 @@ app.use('/api/v1/skills', SkillRoutes);
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
+
+// global error handler
+app.use(globalErrHandler);
 
 export default app;
